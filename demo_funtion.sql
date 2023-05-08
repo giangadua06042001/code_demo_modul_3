c635071ec32c152b7fc10dafc6e2183f533492df
giangadua06042001@gmai.com <abc@gmail.com>
2023-05-08 22:55:04 +0700

    Sua lai

--- demo_funtion.sql
+++ demo_funtion.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE demo_funtion;
-USE demo_funtion;
+CREATE DATABASE demo_function;
+USE demo_function;
 -- Tạo bảng
 CREATE TABLE product(
 proID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -49,59 +49,61 @@ VALUES  (4,'Nguyễn Văn Thành ',31,'Ba Bể- Bắc Kạn ');
 INSERT INTO students(studentID,studentName,Age,Addess)
 VALUES  (5,'Nguyễn Hải Sơn ',29,'Ba Đình - Hà Nội  ');
 CREATE TABLE subject(
-cubID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-subnName VARCHAR (30) NOT NULL,
+subID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+subName VARCHAR (30) NOT NULL,
 program INT DEFAULT 2);
-INSERT INTO subject (cubID,subnName,program)
+INSERT INTO subject (subID,subName,program)
 VALUES (1,'Ngữ Văn',2);
-INSERT INTO subject (cubID,subnName,program)
+INSERT INTO subject (subID,subName,program)
 VALUES (2,'TOÁN',3);
-INSERT INTO subject (cubID,subnName,program)
+INSERT INTO subject (subID,subName,program)
 VALUES (3,'Tiếng Anh',4);
-INSERT INTO subject (cubID,subnName,program)
+INSERT INTO subject (subID,subName,program)
 VALUES (4 ,'Hóa Học',2);
-INSERT INTO subject (cubID,subnName,program)
+INSERT INTO subject (subID,subName,program)
 VALUES (5 ,'Vật lí',1);
 
-CREATE TABLE sroces(
-sroceID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE scores(
+scoreID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 className VARCHAR (60) NOT NULL,
 studentID INT NOT NULL ,
 FOREIGN KEY (studentID) REFERENCES students(studentID),
-cubID INT NOT NULL,
-FOREIGN KEY(cubID) REFERENCES subject(cubID),
+subID INT NOT NULL,
+FOREIGN KEY(subID) REFERENCES subject(subID),
 mark INT NOT NULL
 );
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(1,'10A5',1,1,8);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(2,'10A5',2,1,5);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(3,'10A5',3,1,9);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(4,'10A5',4,1,9);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(5,'10A5',5,1,3);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(6,'10A5',1,2,10);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(7,'10A5',2,2,5);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(8,'10A5',3,2,9);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(9,'10A5',4,2,7);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(10,'10A2',5,3,6);
-INSERT INTO sroces(sroceID,className,studentID,cubID,mark)
-VALUES(11,'10A2',1,4,9);
-SELECT students.studentID,  students.studentName,subject.subnName, sroces.mark ,SUM(mark)
-FROM students JOIN sroces  ON students.studentID=sroces.studentID
-JOIN subject ON sroces.cubID=subject.cubID
-GROUP BY studentID,StudentName,subnName,mark;
-SELECT students.studentID, students.studentName,students.Addess, sroces.mark,
+INSERT INTO scores(scoreID,className,studentID,cubID,mark)
+VALUES(1,'10A5',1,1,8),
+(2,'10A5',2,1,5),
+(3,'10A5',3,1,9),
+(4,'10A5',4,1,9),
+(5,'10A5',5,1,3),
+(6,'10A5',1,2,10),
+(7,'10A5',2,2,5),
+(8,'10A5',3,2,9),
+(9,'10A5',4,2,7),
+(10,'10A2',5,3,6),
+(11,'10A2',1,4,9);
+-- Dùng câu lệnh GROUP BY
+SELECT students.studentID,  students.studentName,subject.subName, scores.mark ,SUM(mark)
+FROM students JOIN scores  ON students.studentID=scores.studentID
+JOIN subject ON scores.subID=subject.subID
+GROUP BY studentID,StudentName,subName,mark;
+
+SELECT students.studentID, students.studentName,students.Addess, 
+COUNT(Addess) FROM demo_funtion.students
+GROUP BY studentID,studentName,Addess;
+
+SELECT students.studentID, students.studentName,students.Addess, scores.mark,
 COUNT(Addess)
-FROM students JOIN sroces ON students.studentID=sroces.studentID
+FROM students JOIN scores ON students.studentID=scores.studentID
 GROUP BY studentID,studentName,Addess,mark;
 
-
+SELECT students.studentID,students.studentName,students.Age,subject.subName,scores.mark,
+SUM(Age) AS "Tong"
+FROM students JOIN scores ON students.studentID=scores.studentID
+JOIN subject ON scores.subID=subject.subID
+WHERE subName='Ngữ Văn'
+GROUP BY studentID, studentName,Age, subName,mark
+HAVING Age>25;
 
